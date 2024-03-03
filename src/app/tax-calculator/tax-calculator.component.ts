4db69ea8b82eb99781284400f31418116a334381
vidyartcr <vidyar.tcr@gmail.com>
2024-03-03 15:16:40 +0530

    stagecommit

---
+++ src/app/tax-calculator/tax-calculator.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tax-calculator',
+  templateUrl: './tax-calculator.component.html',
+  styleUrls: ['./tax-calculator.component.scss']
+})
+export class TaxCalculatorComponent {
+
+}
